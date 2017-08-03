9f35d8fcfb3cae30fae400a46fabff14e0ee2b68
nanfengpo <lsxxxxxx@126.com>
2017-08-03 11:58:56 +0800

    1. 修改了venv 2. 增加了requirement.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nanfengpo/Documents/flask/flask_python"
+set -gx VIRTUAL_ENV "/home/nanfengpo/Documents/flask/flask_hello/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
